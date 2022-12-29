29fa19d930914b66bc39dd63fa11770ba2e91436
stdbob <bobber1@yahoo.com>
2022-12-29 14:27:18 +0200

    ch08 - problem1 unmodifiable rates map

---
+++ src/main/java/com/vpp/ch08/catalog/Price.java
@@ -0,0 +1,39 @@
+package com.vpp.ch08.catalog;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Price {
+
+	private Map<String,Double> rates;
+	private Double value;
+	
+	public Price(Double value) {
+		this.value = value;
+		rates = Map.of(
+		"USD", 1d,
+		"GBP", 0.6,
+		"EUR", 0.8);
+	}
+		
+	public Double convert(String toCurrency) {
+		
+		if (toCurrency.equals("USD")) {
+			return value;
+		}
+		else {
+			Double conversion = rates.get("USD") * rates.get(toCurrency);
+			value = conversion * value;
+			return value;
+		}
+	}
+	
+	public String toString() {
+		return this.value.toString();
+	}
+	
+	public Map<String,Double> getRates() {
+		return rates;
+	}
+	
+}
